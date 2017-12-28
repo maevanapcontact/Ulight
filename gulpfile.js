187114afd3421e400b108fa7449c0a87ed2fbb59
kryaksy <ib.koray@gmail.com>
2017-12-28 06:16:08 +0300

    Adding browserSync task
    - used Beautify plugin for js
    - ulight $ npm install browser-sync --save-dev
    - browserSync task for all js and html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
 // REQUIRES
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 
 // TASKS
 
+// Browser Sync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src/assets'
+    },
+  })
+})
+
 // Sass task
 gulp.task('sass', () => {
   return gulp.src('src/assets/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/assets/css/style.css'));
+    .pipe(sass())
+    .pipe(gulp.dest('src/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 // Watch tasks
-gulp.task('watch',['sass'], () =>{
-  gulp.watch('src/assets/scss/**/*.scss',['sass']);
+gulp.task('watch', ['browserSync', 'sass'], () => {
+  gulp.watch('src/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('src/assets/js/**/*.js', browserSync.reload);
+  gulp.watch('src/assets/*.html', browserSync.reload);
 });
-
-// Default Tasks
-gulp.task('default',['watch']);
